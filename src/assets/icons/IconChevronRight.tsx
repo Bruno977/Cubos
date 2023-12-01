1e2b329f7f1b7c5a03139767c3664f89a0dd402e
Bruno Rodrigues <bruno_151997@hotmail.com>
2023-12-01 19:46:07 -0300

    feat: add icon chevron

---
+++ src/assets/icons/IconChevronRight.tsx
@@ -0,0 +1,13 @@
+export function IconChevronRight({ color }: { color: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="9"
+      height="14"
+      viewBox="0 0 9 14"
+      fill="none"
+    >
+      <path d="M1.5 1L7.5 7L1.5 13" stroke={color} strokeWidth="2" />
+    </svg>
+  );
+}
