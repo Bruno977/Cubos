7976698a496b6457b2dcee17dcf9381087f7cd52
Bruno Rodrigues <bruno_151997@hotmail.com>
2023-11-30 21:19:53 -0300

    fix: children layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,14 +5,17 @@ import { darkTheme } from '../styles/themes/dark';
 import { useToggleThemeContext } from '../contexts/ToggleThemeContext';
 import { lightTheme } from '../styles/themes/light';
 import { GlobalStyle } from '../styles/global';
+import { Outlet } from 'react-router-dom';
 
-export function Layout({ children }: PropsWithChildren) {
+export function Layout() {
   const { theme } = useToggleThemeContext();
   return (
     <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
       <GlobalStyle />
       <Header />
-      <main>{children}</main>
+      <main>
+        <Outlet />
+      </main>
     </ThemeProvider>
   );
 }
