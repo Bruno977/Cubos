ee52efa9a26c34596356b632141d865303bdf2f5
Bruno Rodrigues <bruno_151997@hotmail.com>
2023-12-01 11:16:44 -0300

    style: base style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,22 +1,32 @@
-import { mauve } from '@radix-ui/colors';
 import styled, { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   *{
     margin: 0;
     padding: 0;
+    border: 0;
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
-    font-family: 'Montserrat', sans-serif;
   }
+  html,
   body {
     background-color: ${(props) => props.theme.colors.mauve1};
+    font-family: 'Montserrat', sans-serif;
+    width: 100%;
   }
   a{
     text-decoration: none;
   }
-  ol, ul { list-style: none; }
+  ol, ul {
+     list-style: none; 
+  }
 
+  img {
+    max-width: 100%;
+}
 `;
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding-left: 1.25rem;
+  padding-right: 1.25rem;
+`;
