db6e403cdb8e80ff17749765e45d9239a9e928ef
Bruno Rodrigues <bruno_151997@hotmail.com>
2023-12-03 01:03:36 -0300

    feat: route details

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Layout } from './components/Layout';
+import { DetailMovie } from './pages/DetailMovie';
 
 export function RoutesComponent() {
   return (
@@ -8,6 +9,7 @@ export function RoutesComponent() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/detalhes/:id" element={<DetailMovie />} />
         </Route>
       </Routes>
     </BrowserRouter>
