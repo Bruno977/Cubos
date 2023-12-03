ed6e0d044ddf4bbb1b3b65b5f77b2b9f942bb2d9
Bruno Rodrigues <bruno_151997@hotmail.com>
2023-12-03 01:03:18 -0300

    feat: add types

---
+++ src/types/movie.ts
@@ -0,0 +1,53 @@
+import { GenresProps } from './genres';
+
+export type MovieType = {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: string;
+  budget: number;
+  genres: GenresProps[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: [
+    {
+      id: number;
+      logo_path: string;
+      name: string;
+      origin_country: string;
+    },
+    {
+      id: number;
+      logo_path: null;
+      name: string;
+      origin_country: string;
+    }
+  ];
+  production_countries: [
+    {
+      iso_3166_1: string;
+      name: string;
+    }
+  ];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: [
+    {
+      english_name: string;
+      iso_639_1: string;
+      name: string;
+    }
+  ];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
---
+++ src/types/videos.ts
@@ -0,0 +1,15 @@
+export type VideosType = {
+  id: number;
+  results: {
+    iso_639_1: string;
+    iso_3166_1: string;
+    name: string;
+    key: string;
+    site: string;
+    size: 1080;
+    type: string;
+    official: true;
+    published_at: string;
+    id: string;
+  }[];
+};
